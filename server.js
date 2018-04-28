f5fb29603304fadfabe99ca07964d0f51a068be5
pquantum <pr.pascal.bachmann@gmail.com>
2018-04-28 13:05:56 +0200

    test server

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-var http = require("http");
+const http = require("http");
 
-http.createServer(function(request, response) {
+const server = http.createServer();
+
+server.on("connection", (socket) => {
+  console.log("new connection...");
+});
+
+server.listen(3000);
+
+/*http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
-}).listen(8888);
+}).listen(8888);*/
